e5d184fb7ab3219f75c145b383047aebb17daa5d
criticalAY <mailtoashish693@gmail.com>
2022-10-18 00:01:46 +0530

    image picker commit

--- app/src/main/java/com/ashishyadav/sling/ui/Profile.kt
+++ app/src/main/java/com/ashishyadav/sling/ui/Profile.kt
@@ -1,12 +1,39 @@
 package com.ashishyadav.sling.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import com.ashishyadav.sling.R
 
 class Profile : AppCompatActivity() {
+    lateinit var profileView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        pickImage()
+
+    }
+    private fun pickImage(){
+        profileView = findViewById(R.id.profile_picture)
+        profileView.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            profileView.setImageURI(imageUri)
+        }
     }
 }
