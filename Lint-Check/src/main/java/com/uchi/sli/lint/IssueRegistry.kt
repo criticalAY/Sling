844c01f9b943e81af7130f8db838afa489811352
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-01-26 19:17:33 +0530

    added the lint files

---
+++ Lint-Check/src/main/java/com/uchi/sli/lint/IssueRegistry.kt
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) Ashish Yadav <mailtoashish693@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+package com.uchi.sli.lint_check
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.*
+import com.uchi.sli.lint_check.rules.CopyrightHeaderExists
+import java.util.*
+
+open class IssueRegistry : IssueRegistry() {
+    // Keep this list lexicographically ordered.
+    override val issues: List<Issue>
+        get() {
+            // Keep this list lexicographically ordered.
+            return listOf(
+                CopyrightHeaderExists.ISSUE,
+            )
+        }
+}
