ba66a7cde35cd41597dd02c5b1e30c2a9cfedd33
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2023-01-30 04:55:49 +0530

    Added diff layouts depending on user type

--- Sling-Android/src/main/java/com/uchi/sling/ui/UserDetails.kt
+++ Sling-Android/src/main/java/com/uchi/sling/ui/UserDetails.kt
@@ -21,12 +21,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.uchi.sling.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.uchi.sling.utils.snackbars.showSnackbar
 
 /**
  * A simple [Fragment] subclass.
@@ -34,44 +32,29 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class UserDetails : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val userArgs: UserDetailsArgs by navArgs()
+    var uProfileCode: Int? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_details, container, false)
+        val layoutId: Int = when (userArgs.userTypeArg) {
+            0 -> R.layout.organisation_layout
+            1 -> R.layout.organisation_member_layout
+            else -> R.layout.fragment_user_details
+        }
+        return inflater.inflate(layoutId, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserDetails.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserDetails().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        uProfileCode = userArgs.userTypeArg
+        view.showSnackbar(uProfileCode.toString(), Snackbar.LENGTH_LONG)
+        // TODO: diff codes/methods according to profile type
+        // TODO: Init sql database here and store user data and the type of profile they made
+        // TODO: Save this data to firebase
+        // TODO: Take user to appropriate dashboard
     }
+
 }
